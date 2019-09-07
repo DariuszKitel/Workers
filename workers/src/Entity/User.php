1c7d531804bf6ad4b0a15b5e8602be8c01ba4575
Dariusz Kitel <dariuszkitel@gmail.com>
2019-09-07 22:53:27 +0200

    Creating permissions for change login date and own posts (PUT,POST,GET

--- workers/src/Entity/User.php
+++ workers/src/Entity/User.php
@@ -15,13 +15,32 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ApiResource(
  *     itemOperations={
  *      "get"={
- *          "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
+ *          "access_control"="is_granted('IS_AUTHENTICATED_FULLY')",
+ *          "normalization_context"={
+ *              "groups"={"get"}
+ *              }
+ *          },
+ *     "put"={
+ *              "access_control"="is_granted('IS_AUTHENTICATED_FULLY') and object === user",
+ *              "denormalization_context"={
+ *              "groups"={"put"}
+ *              },
+ *              "normalization_context"={
+ *              "groups"={"get"}
+ *              }
  *          }
  *     },
- *     collectionOperations={"post"},
- *     normalizationContext={
- *      "groups"={"read"}
- *     }
+ *     collectionOperations={
+ *     "post"={
+ *     "denormalization_context"={
+ *              "groups"={"post"}
+ *              },
+ *              "normalization_context"={
+ *              "groups"={"get"}
+ *              }
+ *          }
+ *     },
+ *
  * )
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity("username")
@@ -33,13 +52,13 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"read"})
+     * @Groups({"get"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"read"})
+     * @Groups({"get", "post"})
      * @Assert\NotBlank()
      * @Assert\Length(min="2", minMessage="Imię musi posiadać conajmniej 2 znaki", max="255", maxMessage="Przekroczono limit znaków!")
      */
@@ -47,6 +66,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"put", "post"})
      * @Assert\NotBlank()
      * @Assert\Regex(
      *     pattern="/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{7,}/",
@@ -56,6 +76,7 @@ class User implements UserInterface
     private $password;
 
     /**
+     * @Groups({"put", "post"})
      * @Assert\NotBlank()
      * @Assert\Expression(
      *     "this.getPassword() === this.getRetypedPassword()",
@@ -67,7 +88,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"read"})
+     * @Groups({"get", "post", "put"})
      * @Assert\NotBlank()
      * @Assert\Length(min="2", minMessage="Imię musi posiadać conajmniej 2 znaki", max="255", maxMessage="Przekroczono limit znaków!")
      */
@@ -75,6 +96,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"post", "put"})
      * @Assert\NotBlank()
      * @Assert\Email()
      * @Assert\Length(min="6", max="255", minMessage="Ten adres jest za krótki!", maxMessage="Przekroczono limt znaków!")
@@ -83,13 +105,13 @@ class User implements UserInterface
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\WorkPost", mappedBy="author")
-     * @Groups({"read"})
+     * @Groups({"get"})
      */
     private $posts;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Question", mappedBy="author")
-     * @Groups({"read"})
+     * @Groups({"get"})
      */
     private $question;
 
