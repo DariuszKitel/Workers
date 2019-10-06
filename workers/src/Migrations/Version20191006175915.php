fc5ec56cd0250e625afe8167eef683fc480441e3
Dariusz Kitel <dariuszkitel@gmail.com>
2019-10-06 20:30:12 +0200

    Created new reset password functionality without last error (DB selfbug)

--- workers/src/Migrations/Version20191006175915.php
+++ workers/src/Migrations/Version20191006175915.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191005230707 extends AbstractMigration
+final class Version20191006175915 extends AbstractMigration
 {
     public function getDescription() : string
     {
